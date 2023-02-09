828eb7262c2e5ef15c87a0cfb5b410137c23bd94
Temitayo Oguntide <temitech5@gmail.com>
2023-02-09 22:15:54 +0100

    new changes

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ColorPickerGame v-if="currentLesson === 'color-picker-game'" />
+  <!-- <Recap_01_02_2023 v-else /> -->
+  <ColorPickerGameComposable />
+</template>
+
+<script>
+import ColorPickerGameComposable from "./components/ColorPickerGameComposable.vue";
+import ColorPickerGame from "./components/ColorPickerGame.vue";
+// import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
+export default {
+  components: {
+    ColorPickerGame,
+    // Recap_01_02_2023,
+    ColorPickerGameComposable,
+  },
+  data() {
+    return {
+      currentLesson: "color-picker-game",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/ColorPickerGameComposable.vue
@@ -0,0 +1,34 @@
+<template>
+	<h1>Color Picker Game in Composable format</h1>
+  
+	<div>{{ message }}</div>
+  
+	<button v-for="color in colors" :key="color" @click="matchColor(color)">
+	  {{ color }}
+	</button>
+  </template>
+  
+  <script>
+  export default {
+	name: "ColorPickerGame",
+	data() {
+	  return {
+		  colors: ["green", "red", "blue", "purple"],
+		  message: "Pick a color...",
+	  };
+	},
+	methods: {
+	  matchColor (value) {
+		// do a random color based on the array index;
+		const randomNumber = Math.floor(Math.random() * 4); //between 1 - 4
+		if (this.colors[randomNumber] === value) {
+		  this.message = `You win... [answer: ${this.colors[randomNumber]}]`;
+		  return;
+		}
+		this.message = `You loose [answer: ${this.colors[randomNumber]}]`;
+	  }
+	}
+  };
+  </script>
+  
+  <style scoped></style>
